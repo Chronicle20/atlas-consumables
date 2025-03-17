7a06a4fc50825ab93588c20060a07ca07bb0ff8b
Chronicle20 <a.chronicle.20@gmail.com>
2025-03-17 18:03:26 -0400

    Handle HP% and MP% recovery.

--- atlas.com/consumables/character/processor.go
+++ atlas.com/consumables/character/processor.go
@@ -4,8 +4,21 @@ import (
 	"atlas-consumables/kafka/producer"
 	"context"
 	_map "github.com/Chronicle20/atlas-constants/map"
+	"github.com/Chronicle20/atlas-model/model"
+	"github.com/Chronicle20/atlas-rest/requests"
 	"github.com/sirupsen/logrus"
 )
+
+func GetById(l logrus.FieldLogger) func(ctx context.Context) func(decorators ...model.Decorator[Model]) func(characterId uint32) (Model, error) {
+	return func(ctx context.Context) func(decorators ...model.Decorator[Model]) func(characterId uint32) (Model, error) {
+		return func(decorators ...model.Decorator[Model]) func(characterId uint32) (Model, error) {
+			return func(characterId uint32) (Model, error) {
+				p := requests.Provider[RestModel, Model](l, ctx)(requestById(characterId), Extract)
+				return model.Map(model.Decorate(decorators))(p)()
+			}
+		}
+	}
+}
 
 func ChangeHP(l logrus.FieldLogger) func(ctx context.Context) func(m _map.Model, characterId uint32, amount int16) error {
 	return func(ctx context.Context) func(m _map.Model, characterId uint32, amount int16) error {
